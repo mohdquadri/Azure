5cb0b38badaee6f2ba191d3bfb0a304d08c926e0
mohdquadri <50426301+mohdquadri@users.noreply.github.com>
2024-05-23 09:41:11 -0500

    Create mailboxcount.ps1

---
+++ mailboxcount.ps1
@@ -0,0 +1,16 @@
+    $Credentials = Get-Credential -UserName rizwan.quadri@copart.com -Message "Enter your credentials"
+ # Install Exchange Online Management Module if not already installed
+    if (-not (Get-Module ExchangeOnlineManagement -ListAvailable)) {
+        Install-Module -Name ExchangeOnlineManagement -Force -Scope CurrentUser
+    }
+
+    # Import Exchange Online Management Module
+    Import-Module ExchangeOnlineManagement -Force
+    
+    # Connect to Exchange Online
+    Connect-ExchangeOnline -UserPrincipalName rizwan.quadri@copart.com -ShowProgress $true
+
+    Write-Host "Connected to Office 365 as $UserPrincipalName"
+    write-host " "
+
+    Get-mailbox -ResultSize unlimited | Measure-Object
